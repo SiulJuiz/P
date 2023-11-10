9fc084438e14660f12442140507a80dd418863a0
luisl <luisl@LAPTOP-JKP8V6TU>
2023-11-09 21:32:23 -0500

    Fórmula general números enteros positivos

---
+++ OneDrive/Documentos/NetBeansProjects/Prueba/src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main (String []args){
+        int var=0;
+        if(var==0){
+            int nuevoV=100;
+        }
+        System.out.println(nuevoV);
+    }
+}
---
+++ OneDrive/Documentos/NetBeansProjects/Prueba/src/main/java/com/mycompany/prueba/Prueba.java
@@ -0,0 +1,10 @@
+
+
+package com.mycompany.prueba;
+
+import java.util.Scanner;
+
+public class Prueba {
+  private int nota;
+  Prueba miObjeto=new Prueba();
+}
